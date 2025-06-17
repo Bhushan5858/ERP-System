7ac095a82b03ddc9bd685c6e0bae7d0fb2371e19
Bhushan5858 <patilpavan5858@gmail.com>
2025-06-17 17:08:36 +0530

    sidebar procces start

--- erp-system/src/Components/Sidebar.jsx
+++ erp-system/src/Components/Sidebar.jsx
@@ -1,9 +1,25 @@
-import react from 'react';
+
+import {useAuthUserStore } from '../Store/AuthUserStore.js';
 
 const Sidebar =()=>{
+    const {authUser} = useAuthUserStore();
+
+    console.log(authUser)
     return(<>
- <div className='bg-gray-800 h-screen w-[20%]'>
-    
+ <div className='bg-gray-800 h-screen w-[20%] flex flex-col'>
+        <div className='profile'>
+            
+           <div className='flex justify-center border-'>
+            <img src='https://images.pexels.com/photos/1575983/pexels-photo-1575983.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='profile'/>
+           </div>
+           <div className='flex justify-center'>
+            <h1>Admin</h1>
+           </div>
+           <div className='flex justify-center'>
+            <p>admin@example.com</p>
+           </div>
+        </div>
+
  </div>
     </>)
 }
